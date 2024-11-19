0b63c7f07a378e0663cebdd729b82c52cca5b101
Lee Hyun-jun <87108378+tunatuna123@users.noreply.github.com>
2024-11-19 19:58:00 +0900

    test file

---
+++ 본계정/1_test.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define min(a, b)      (((a) < (b)) ? (a) : (b))
+#define max(a, b)      (((a) > (b)) ? (a) : (b))
+#define pi 3.14159265359
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    double k;
+    cin >> k;
+
+    cout << 4.0 / 3.0 * pi * pow(k, 3) / 1000 << endl;
+    cout << 4 / 3 * pi * pow(k, 3) / 1000 << endl;
+    cout << 4/3 << endl;
+    cout << 4.0/3.0 << endl;
+    cout << 4/3.0 << endl;
+    cout << 4.0/3 << endl;
+    return 0;
+}
